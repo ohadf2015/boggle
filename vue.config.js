bea858eb29f620096275098e01d9589767a91fe0
ohad fisher <ohad@appdome.com>
2021-09-18 01:59:44 +0300

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,17 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/boggle/'
-    : '/'
+    : '/',
+
+  pluginOptions: {
+    i18n: {
+      locale: 'he',
+      fallbackLocale: 'en',
+      localeDir: 'locales',
+      enableLegacy: false,
+      runtimeOnly: false,
+      compositionOnly: false,
+      fullInstall: true
+    }
+  }
 }
